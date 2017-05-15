47484e4d5cf3f9c0619dce4ead63ede26e9d41ae
Taylor Bell <taylorbell@gmail.com>
2017-05-15 14:43:04 -0600

    build icons

---
+++ egghead/comment-alert.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import Icon from 'react-icon-base'
+
+const EggheadCommentAlert = props => (
+    <Icon viewBox="0 0 24 24" {...props}>
+        <g>
+    <g id="Admin" stroke="none" strokeWidth="1" fillRule="evenodd">
+        <g id="Action-Cards" transform="translate(-898.000000, -369.000000)">
+            <g id="&#xFF1F;button_askforchanges" transform="translate(890.000000, 363.000000)">
+                <g id="ico_askforchanges" transform="translate(8.000000, 6.000000)">
+                    <g>
+                        <path d="M19.2,3 L4.8,3 C3.81000008,3 3.00899999,3.81000008 3.00899999,4.8 L3,21 L6.6,17.4 L19.2,17.4 C20.1900003,17.4 21,16.5900003 21,15.6 L21,4.8 C21,3.81000008 20.1900003,3 19.2,3 Z M13,11 L11,11 L11,6 L13,6 L13,11 Z M13,14 L11,14 L11,12 L13,12 L13,14 Z"/>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</g>
+    </Icon>
+)
+
+export default EggheadCommentAlert
