053c4df0429b7adef6dee4f9cdda487a727b369f
Taylor Bell <taylorbell@gmail.com>
2017-05-15 19:41:04 -0600

    Add icons (#3)
    * ignore ds_store
    
    * add icons
    
    * build icons
    
    * v3.5.0
    
    * upstream

---
+++ egghead/alert.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import Icon from 'react-icon-base'
+
+const EggheadAlert = props => (
+    <Icon viewBox="0 0 23 23" {...props}>
+        <g>
+    <g id="Admin" stroke="none" strokeWidth="1" fillRule="evenodd">
+        <g id="Action-Cards" transform="translate(-798.000000, -458.000000)">
+            <g id="&#x1F44D;button_retire" transform="translate(790.000000, 451.000000)">
+                <g id="ico_retire" transform="translate(8.000000, 7.000000)">
+                    <g>
+                        <path d="M10.4493445,3.79716287 C11.0286108,2.73010755 11.9723966,2.73838029 12.5474438,3.79739913 L19.7533397,17.0679254 C20.3328216,18.1351112 19.8543658,19.0001295 18.7044852,19 L4.29537377,18.9983774 C3.13662552,18.998247 2.67129564,18.124956 3.24612897,17.0660666 L10.4493445,3.79716287 Z M10.5,8 L10.5,13 L12.5,13 L12.5,8 L10.5,8 Z M10.5,14.375 L10.5,16.375 L12.5,16.375 L12.5,14.375 L10.5,14.375 Z"/>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</g>
+    </Icon>
+)
+
+export default EggheadAlert
