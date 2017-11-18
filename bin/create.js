a3dc73775f3d8b5385cb436dd1e4cc9c44e495f8
AkashGutha <akash.gutha@outlook.com>
2017-11-18 18:42:01 +0530

    automated camelcase issue

--- bin/create.js
+++ bin/create.js
@@ -1,48 +1,69 @@
-var fs = require('fs');
-var cheerio = require('cheerio');
-var camelcase = require('camelcase');
-var capitalize = require('capitalize');
+var fs = require("fs");
+var cheerio = require("cheerio");
+var camelcase = require("camelcase");
+var capitalize = require("capitalize");
 var components = {};
 var types = {};
-var _ = require('underscore');
-var glob = require('glob');
-var path = require('path');
-var rootDir = path.join(__dirname, '..');
-var attrs = ['xlink:href', 'clip-path', 'fill-opacity', 'fill'];
-var cleanAtrributes = function($el, $) {
-    _.each(attrs, function(attr) {
-        $el.removeAttr(attr);
-    });
-    if($el.children().length === 0) {
-        return false;
-    }
+var _ = require("underscore");
+var glob = require("glob");
+var path = require("path");
+var rootDir = path.join(__dirname, "..");
+var attrs = ["xlink:href", "clip-path", "fill-opacity", "fill"];
 
-    $el.children().each(function(index, el) {
-        cleanAtrributes($(el), $);
-    });
+var cleanAtrributes = function($el, $) {
+  _.each(attrs, function(attr) {
+    $el.removeAttr(attr);
+  });
+  if ($el.children().length === 0) {
+    return false;
+  }
+
+  $el.children().each(function(index, el) {
+    cleanAtrributes($(el), $);
+  });
 };
 
-glob(rootDir + '/icons/*/*.svg', function(err, icons) {
-    icons.forEach(function(iconPath){
-        var id = path.basename(iconPath, '.svg');
-        var svg = fs.readFileSync(iconPath, 'utf-8');
-        $ = cheerio.load(svg,{
-            xmlMode: true
-        });
-        var $svg = $('svg');
-        cleanAtrributes($svg, $);
-        var iconSvg = $svg.html();
-        var viewBox = $svg.attr('viewBox');
-        var folder = iconPath.replace(path.join(rootDir, 'icons') + '/', '').replace( '/' + path.basename(iconPath), '');
-        var type = capitalize(camelcase(folder));
-        var name = type + capitalize(camelcase(id));
-        var location = iconPath.replace(path.join(rootDir, 'icons'), '').replace('.svg', '.js');
-        components[name] = location;
-        if (!types[folder]) {
-            types[folder] = {};
-        }
-        types[folder][name] = location;
-        var component = `
+var camelCaseAttrs = ["fill-rule", "stroke-width"];
+var camelcaseAttributes = function($el, $) {
+  _.each(camelCaseAttrs, function(attr) {
+    $el.attr(camelcase(attr), $el.attr(attr));
+    $el.removeAttr(attr);
+  });
+  if ($el.children().length === 0) {
+    return false;
+  }
+
+  $el.children().each(function(index, el) {
+    camelcaseAttributes($(el), $);
+  });
+};
+
+glob(rootDir + "/icons/*/*.svg", function(err, icons) {
+  icons.forEach(function(iconPath) {
+    var id = path.basename(iconPath, ".svg");
+    var svg = fs.readFileSync(iconPath, "utf-8");
+    $ = cheerio.load(svg, {
+      xmlMode: true
+    });
+    var $svg = $("svg");
+    cleanAtrributes($svg, $);
+    camelcaseAttributes($svg, $);
+    var iconSvg = $svg.html();
+    var viewBox = $svg.attr("viewBox");
+    var folder = iconPath
+      .replace(path.join(rootDir, "icons") + "/", "")
+      .replace("/" + path.basename(iconPath), "");
+    var type = capitalize(camelcase(folder));
+    var name = type + capitalize(camelcase(id));
+    var location = iconPath
+      .replace(path.join(rootDir, "icons"), "")
+      .replace(".svg", ".js");
+    components[name] = location;
+    if (!types[folder]) {
+      types[folder] = {};
+    }
+    types[folder][name] = location;
+    var component = `
 import React from 'react'
 import Icon from 'react-icon-base'
 
@@ -53,22 +74,25 @@ const ${name} = props => (
 )
 
 export default ${name}
-`
+`;
 
-        fs.writeFileSync(path.join(rootDir, location), component, 'utf-8');
-        console.log(path.join('.', location));
-    });
-    _.each(types, function(components, folder) {
-        var iconsModule = _.map(components, function(loc, name){
-            loc = loc.replace('.js', '');
-            loc = loc.replace('/'+folder, '');
-            loc = "." + loc;
-            return `export ${name} from '${loc}';`;
-        }).join('\n') + '\n';
-        fs.writeFileSync(path.join(rootDir, folder , 'index.js'), iconsModule, 'utf-8');
-        console.log(path.join('.', folder, 'index.js'));
-    });
-    console.log("IconBase.js");
+    fs.writeFileSync(path.join(rootDir, location), component, "utf-8");
+    console.log(path.join(".", location));
+  });
+  _.each(types, function(components, folder) {
+    var iconsModule =
+      _.map(components, function(loc, name) {
+        loc = loc.replace(".js", "");
+        loc = loc.replace("/" + folder, "");
+        loc = "." + loc;
+        return `export ${name} from '${loc}';`;
+      }).join("\n") + "\n";
+    fs.writeFileSync(
+      path.join(rootDir, folder, "index.js"),
+      iconsModule,
+      "utf-8"
+    );
+    console.log(path.join(".", folder, "index.js"));
+  });
+  console.log("IconBase.js");
 });
-
-
